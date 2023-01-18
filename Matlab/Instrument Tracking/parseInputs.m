d62d5ff32e47b53beb6f3de2e2a8904de1c508df
Xianzhong Xie <73322475+happyheroboy@users.noreply.github.com>
2023-01-18 13:25:41 +0800

    Delete Matlab directory

--- Matlab/Instrument Tracking/parseInputs.m
+++
@@ -1,9 +0,0 @@
-function [points1, points2, camMatrix1, camMatrix2] = ...
-    parseInputs(matchedPoints1, matchedPoints2, varargin)
-
-narginchk(3, 4);
-[points1, points2] = parsePoints(matchedPoints1, matchedPoints2);
-[P1, P2] = parseCameraMatrices(varargin{:});
-camMatrix1 = cast(P1, 'like', points1)';
-camMatrix2 = cast(P2, 'like', points2)';
-
--- Matlab/Marker Detection/reset_toolbox.m
+++
@@ -1,5 +0,0 @@
-% clear;
-
-addpath(genpath('./Control'));
-addpath(genpath('./Data'));
-addpath(genpath('./View'));
